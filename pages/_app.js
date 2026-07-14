91e88c9a0c4271a9f8805313d2c62fc08eb9938b
Your Name <you@example.com>
2026-07-14 17:08:21 +0530

    Configure react-hot-toast globally in Next.js app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
